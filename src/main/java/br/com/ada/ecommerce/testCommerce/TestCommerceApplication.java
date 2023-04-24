f4902b924ff7dc1b9e56e7efbf83ee396dd2a076
Ivo Augusto <ivomudo@gmail.com>
2023-04-23 22:30:42 -0300

    first commit

---
+++ src/main/java/br/com/ada/ecommerce/testCommerce/TestCommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.ecommerce.testCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ada/ecommerce/testCommerce/TestCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.ecommerce.testCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
